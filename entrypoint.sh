a17dfb50c8b7520d193ae498a9d39fb3f239b88a
Matthias Holetzko <mholetzko@gmx.net>
2021-10-15 18:55:33 +0200

    fix: fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 
 
-echo "Received who-to-greet: $1"
+echo "Received data path: $1"
 echo "Received upload-to-wiki: $2"
-python3 -m src/convert_junit_to_md.py ./result.xml TESTRUN-123 www.google.com
-path-to-generated-md="conterved.md"
+python3 src/convert_junit_to_md.py $1 TESTRUN-123 www.google.com
+
+path_to_generated_md="conterved.md"
 
