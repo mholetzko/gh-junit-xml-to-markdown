e528d8e4770d712aedd9667331b4bb746ffa0dd6
Matthias Holetzko <mholetzko@gmx.net>
2021-10-15 18:58:42 +0200

    fix:...

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 
 echo "Received data path: $1"
 echo "Received upload-to-wiki: $2"
-source ./env/bin/activate && python3 src/convert_junit_to_md.py $1 TESTRUN-123 www.google.com
+source /app/env/bin/activate && python3 src/convert_junit_to_md.py $1 TESTRUN-123 www.google.com
 
 path_to_generated_md="conterved.md"
 
