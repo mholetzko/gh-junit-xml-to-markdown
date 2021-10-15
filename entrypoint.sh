831e74f0bc88b7f06d63f965fbc1041118918fd9
Matthias Holetzko <mholetzko@gmx.net>
2021-10-15 23:42:59 +0200

    outname

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 
 echo "Received data path: $1"
 echo "Received result path: $2"
+echo "Received result name: $3"
 source /app/env/bin/activate && python3 src/convert_junit_to_md.py $1 TESTRUN-123 www.google.com
 ls -al
 pwd
@@ -10,12 +11,15 @@ pwd
 echo "Print md"
 cat converted.md
 
+echo "Rename"
+mv ./converted.md ./$3
+
 echo "create outdir"
 mkdir ./$2
 
 echo "copy"
-mv converted.md ./$2/converted.md
+mv ./$3 ./$2/$3
 
-out=./$2/converted.md
+out=./$2/$3
 echo "::set-output name=path_to_generated_md::$out"
 
