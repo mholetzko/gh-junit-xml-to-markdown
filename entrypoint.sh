ea38b4954e3ce57eef21838b9efbdc2f5d743468
Matthias Holetzko <mholetzko@gmx.net>
2021-10-15 19:02:09 +0200

    fix: set output

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo "Received data path: $1"
 echo "Received upload-to-wiki: $2"
 source /app/env/bin/activate && python3 src/convert_junit_to_md.py $1 TESTRUN-123 www.google.com
 
-path_to_generated_md="conterved.md"
+echo "::set-output name=path_to_generated_md::conterved.md"
 
