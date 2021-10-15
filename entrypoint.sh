37bbb2a9dfbea3e3ff9e84cb5cb1f654fd45f74f
Matthias Holetzko <mholetzko@gmx.net>
2021-10-15 23:37:26 +0200

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 
 echo "Received data path: $1"
-echo "Received upload-to-wiki: $2"
+echo "Received result path: $2"
 source /app/env/bin/activate && python3 src/convert_junit_to_md.py $1 TESTRUN-123 www.google.com
 ls -al
 pwd
 cat converted.md
+mv converted.md $2/converted.md
 
-out="converted.md"
+out=$2/converted.md
 echo "::set-output name=path_to_generated_md::$out"
 
