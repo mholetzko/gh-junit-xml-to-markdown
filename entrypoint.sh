0bf011b873d6a29dfed82320f58c3f3ac0450ea2
Matthias Holetzko <mholetzko@gmx.net>
2021-10-15 19:08:49 +0200

    pwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ echo "Received data path: $1"
 echo "Received upload-to-wiki: $2"
 source /app/env/bin/activate && python3 src/convert_junit_to_md.py $1 TESTRUN-123 www.google.com
 ls -al
+pwd
+cat converted.md
 echo "::set-output name=path_to_generated_md::conterved.md"
 
