2d250b781023fc3e4ffa62c75b81c899726440aa
Matthias Holetzko <mholetzko@gmx.net>
2021-10-15 17:30:28 +0200

    fix: not required in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh -l
 
-echo "Create venv"
-python3 -m venv env
-echo "Activate venv"
-source ./env/bin/activate
-echo "Install requirements"
 
 echo "Received who-to-greet: $1"
 echo "Received upload-to-wiki: $2"
