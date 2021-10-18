f94635cff8e00f08e28d80595d12ade9eacba853
Matthias Holetzko <mholetzko@gmx.net>
2021-10-18 13:00:50 +0200

    update - with failure

--- src/convert_junit_to_md.py
+++ src/convert_junit_to_md.py
@@ -2,6 +2,14 @@ from junitparser import JUnitXml
 from mdutils.mdutils import MdUtils
 import sys 
 
+def getPassedFailedStr(hasFailures:bool)-> str:
+    if hasFailures:
+        return "📛"
+    else:
+        return "🏔️"
+
+
+
 FILEPATH=sys.argv[1]
 SUITENAME=sys.argv[2]
 URL=sys.argv[3]
@@ -9,13 +17,20 @@ URL=sys.argv[3]
 mdFile = MdUtils(file_name='converted',title=f'{SUITENAME}')
 
 mdFile.new_paragraph(f"Please find the detailed report here {URL}")
-
+# Most ugly piece of code I wrote in a while 😃
 xml = JUnitXml.fromfile(FILEPATH)
 for suite in xml:
-    mdFile.new_header(level=3, title=" 🟥 Testsuite: "+suite.name+ f" [{max(suite.tests-suite.failures,0)} / {suite.tests}]",add_table_of_contents='n')  # style is set 'atx' format by default.   
+    mdFile.new_header(level=3, title=f" {getPassedFailedStr(suite.failures>0)} Testsuite: "+suite.name+ f" [{max(suite.tests-suite.failures,0)} / {suite.tests}]",add_table_of_contents='n')  # style is set 'atx' format by default.   
     caseList=[]
     for case in suite:
-        caseList.append(" 🟩 [time: "+str(case.time)+"] Scenario: "+case.name)
+        failingList = []
+        for failures in case:
+            print(failures.message)
+            failingList.append(f" **[failure]:<br> "+str(failures.message)+"**")
+        caseList.append(f" {getPassedFailedStr(len(failingList)>0)} [time: "+str(case.time)+"] Scenario: "+case.name)
+        if len(failingList)>0:
+            caseList.append(*failingList)
+
     mdFile.new_list(items=caseList)   # style is set 'atx' format by default.   
 
 mdFile.create_md_file()
